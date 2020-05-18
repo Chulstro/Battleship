351b24ade8f7686338efbf7290d1cca66adb7770
Chandler Hulstrom <chulstro11@gmail.com>
2020-05-18 12:53:23 -0600

    New runner ideas

--- lib/runner.rb
+++ lib/runner.rb
@@ -5,28 +5,48 @@ require 'pry'
 
 
 class Runner
-  attr_reader
+  attr_reader :player_1,
+              :player_2,
+              :cruiser,
+              :submarine,
+              :board
 
   def initialize(player_1, player_2)
     @player_1 = player_1
     @player_2 = player_2
-  end
-
-  def start
     @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
     @board = Board.new
     @board.cells
-    place_ships
-    puts "I have placed my defenses!"
-    puts "Fire when ready"
-    shot = gets.chomp
-    @board.cells[shot].fire_upon
-    @board.render
+  end
+
+  def start
+    initial_statement
+    place_ships_predetermined
+    # shot = gets.chomp
+    # @board.cells[shot].fire_upon
+    # @board.render
+  end
+
+  def place_ships_predetermined
+    @board.place(@cruiser, ["A2", "A3", "A4"])
+    @board.place(@submarine, ["C2", "D2"])
   end
 
   def place_ships
-    @board.place(@cruiser, ["A2", "A3", "A4"])
-    @board.place(@submarine, ["C2", "D2"])
+    @board.place(@cruiser, RANDOM)
+    @board.place(@submarine, RANDOM)
+  end
+
+  def random_cells(number)
+    cell_keys = @board.cells.keys
+    direction = (1..2).sample
+    cell_keys.sample
+    if coordinates == @board.valid_placement?
+  end
+
+  def initial_statement
+    puts "Welcome to BATTLESHIP"
+    puts "Enter p to play. Enter q to quit."
   end
 end
--- test/runner_test.rb
+++ test/runner_test.rb
@@ -2,4 +2,21 @@ require 'minitest/autorun'
 require './lib/runner'
 require 'pry'
 
-binding.pry
+class RunnerTest < Minitest::Test
+
+  def setup
+    @runner = Runner.new("Chan", "Computer")
+  end
+
+  def test_it_exists
+    assert_instance_of Runner, @runner
+  end
+
+  def test_it_places_ships
+    @runner.place_ships_predetermined
+    assert_equal "  1 2 3 4 \nA . S S S \nB . . . . \nC . S . . \nD . S . .", @runner.board.render(true)
+  end
+
+  def test
+  end
+end
